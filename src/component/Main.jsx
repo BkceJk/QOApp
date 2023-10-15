0839a0307264567d5e931574bf90249d76ac6fd7
ErikSanzana <ersanzana@gmail.com>
2023-10-15 18:16:06 -0300

    cambio a main.jsx

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -18,7 +18,7 @@ const Principal = () => {
             QOGame
         </StyledText>
         <StyledText>
-        Quimica Organica Game
+        Quimica Organica Gay
         </StyledText>
     </View>
 </View>
